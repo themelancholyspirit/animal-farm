33a23508d57c079d8150ecc17bb5efdd969e198f
Guga <Masterrrguga@gmail.com>
2025-02-24 17:06:00 +0400

    Fix db related stuff

--- animal-farm-backend/src/app.module.ts
+++ animal-farm-backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AnimalsModule } from './animals/animals.module';
 import { PigStatusModule } from './pig-status/pig-status.module';
 import { MusicModule } from './music/music.module';
@@ -10,13 +12,17 @@ import { PigStatusEntity } from './entities/pig-status.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: process.env.DB_PATH || '/app/data/db.sqlite',
       entities: [Animal, PigStatusEntity],
       synchronize: true,
+      logging: true,
     }),
+    TypeOrmModule.forFeature([Animal]),
     AnimalsModule,
     PigStatusModule,
     MusicModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
