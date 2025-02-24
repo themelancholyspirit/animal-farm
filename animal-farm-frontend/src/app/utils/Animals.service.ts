88772cde7851b18d63e3ee89f69d0a26defc1af2
Guga <Masterrrguga@gmail.com>
2025-02-24 17:05:06 +0400

    Modify AnimalsService to allow dynamic URL's

--- animal-farm-frontend/src/app/utils/Animals.service.ts
+++ animal-farm-frontend/src/app/utils/Animals.service.ts
@@ -1,15 +1,14 @@
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class AnimalsService {
-    private apiUrl = 'http://localhost:3000/api/animals';
+    private apiUrl = `${environment.apiUrl}/animals`;
 
     constructor(private http: HttpClient) {}
 
